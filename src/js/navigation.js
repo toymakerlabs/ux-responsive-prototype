6c6a893dbdd0a1cdc84f8bc755dcd641f162aa42
John Faithorn <john.faithorn@futurenet.com>
2017-06-03 15:41:02 -0700

    revise navbar nav drawer

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,22 +1,30 @@
 //https://stackoverflow.com/questions/33611812/javascript-es6-export-const-vs-export-default
 
 export default function navigation() {
-	const button = $("#sidebar-button")
-	const main = $("#main-container");
-	const close = $("#sidebar-close");
+	const button = $("#drawer-button");
+	const close = $("#drawer-close");
+	const main = $("#main");
+	const nav = $("#main-nav");
+	const page = $("#page");
+	const drawer = $("#drawer");
 
 	button.on("click",function(e){
 		e.preventDefault();
 		e.stopPropagation();
 		console.log("clicked");
 		main.toggleClass("open");
+		nav.toggleClass("open");
+		page.toggleClass("open");
+		drawer.toggleClass("open");
 	})
 
 	close.on("click",function(e){
 		e.preventDefault();
 		e.stopPropagation();
-		console.log("clicked");
 		main.removeClass("open");
+		nav.removeClass("open");
+		page.removeClass("open");
+		drawer.removeClass("open");
 	})
 
 }
